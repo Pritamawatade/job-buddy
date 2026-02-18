d5832ee2860fe813dbaca2aa42a343b36d4f90e6
pritamawatade <pritamawatade5@gmail.com>
2026-02-18 16:39:25 +0530

    added register controller

---
+++ backend/src/modules/auth/auth.controller.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express"
+import ApiError from "../../utils/ApiError";
+import { prisma } from "../../config/postgres";
+import { hashPassword } from "../../utils/password";
+const register = async(req: Request, res: Response)=>{
+const {name, email, password}: {name: string, email: string, password: string} = req.body;
+
+if(!name || !email  || !password){
+    return new ApiError(400, "All fields are required")
+}
+try {
+    const hashedPassword: string = await hashPassword(password);
+    const user = await prisma.user.create({
+        data:{
+            name,
+            email,
+            password: hashedPassword
+        }
+    })
+    
+    return res.status(201).json({
+        success: true,
+        message: "User created successfully",
+        user: user
+    })
+} catch (error) {
+    return new ApiError(500, `Error at register controller ${error as string}`)
+}
+}
+
+export {
+    register
+}
--- backend/src/modules/auth/auth.routes.ts
+++ backend/src/modules/auth/auth.routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
+import { register } from './auth.controller';
 
 const authRoutes = Router();
 
+authRoutes.post('/register', register);
 // TODO: Add auth routes here
 
 export default authRoutes;

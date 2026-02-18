e76028d2de1d7fdf9b18193155fd55affaa4fd68
pritamawatade <pritamawatade5@gmail.com>
2026-02-18 17:02:23 +0530

    feat: added first user

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,33 +1,31 @@
-import { Request, Response } from "express"
-import ApiError from "../../utils/ApiError";
-import { prisma } from "../../config/postgres";
-import { hashPassword } from "../../utils/password";
-const register = async(req: Request, res: Response)=>{
-const {name, email, password}: {name: string, email: string, password: string} = req.body;
+import { Request, Response } from 'express';
+import ApiError from '../../utils/ApiError';
+import { prisma } from '../../config/postgres';
+import { hashPassword } from '../../utils/password';
+const register = async (req: Request, res: Response) => {
+  const { name, email, password }: { name: string; email: string; password: string } = req.body;
 
-if(!name || !email  || !password){
-    return new ApiError(400, "All fields are required")
-}
-try {
+  if (!name || !email || !password) {
+    return new ApiError(400, 'All fields are required');
+  }
+  try {
     const hashedPassword: string = await hashPassword(password);
     const user = await prisma.user.create({
-        data:{
-            name,
-            email,
-            password: hashedPassword
-        }
-    })
-    
-    return res.status(201).json({
-        success: true,
-        message: "User created successfully",
-        user: user
-    })
-} catch (error) {
-    return new ApiError(500, `Error at register controller ${error as string}`)
-}
-}
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
 
-export {
-    register
-}
+    return res.status(201).json({
+      success: true,
+      message: 'User created successfully',
+      user: user,
+    });
+  } catch (error) {
+    return new ApiError(500, `Error at register controller ${error as string}`);
+  }
+};
+
+export { register };
